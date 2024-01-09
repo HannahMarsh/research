cf99df397a111d236273146ea171f587d6374ab4
Hannah Marsh <hanmarsh@icloud.com>
2024-01-09 13:50:41 -0500

    minor change

--- b/config/config.go
+++ b/config/config.go
@@ -6,62 +6,63 @@ import (
 )
 
 type Config struct {
-	InsertStart                                  int64   `yaml:"InsertStart"`
-	InsertCount                                  int64   `yaml:"InsertCount"`
-	OperationCount                               int64   `yaml:"OperationCount"`
-	RecordCount                                  int64   `yaml:"RecordCount"`
-	Workload                                     string  `yaml:"Workload"`
-	ThreadCount                                  int64   `yaml:"ThreadCount"`
-	Target                                       int64   `yaml:"Target"` // target operations per second
-	MaxExecutionTime                             int64   `yaml:"MaxExecutionTime"`
-	WarmUpTime                                   int64   `yaml:"WarmUpTime"`
-	DoTransactions                               bool    `yaml:"DoTransactions"`
-	Status                                       string  `yaml:"Status"`
-	Label                                        string  `yaml:"Label"`
 	BatchSize                                    int     `yaml:"BatchSize"`
-	TableName                                    string  `yaml:"TableName"`
-	FieldCount                                   int64   `yaml:"FieldCount"`
-	FieldLengthDistribution                      string  `yaml:"FieldLengthDistribution"`
-	FieldLength                                  int64   `yaml:"FieldLength"`
-	FieldLengthHistogramFile                     string  `yaml:"FieldLengthHistogramFile"`
-	ReadAllFields                                bool    `yaml:"ReadAllFields"`
-	WriteAllFields                               bool    `yaml:"WriteAllFields"`
+	CassandraCluster                             string  `yaml:"CassandraCluster"`
+	CassandraConnections                         int     `yaml:"CassandraConnections"`
+	CassandraKeyspace                            string  `yaml:"CassandraKeyspace"`
+	CassandraPassword                            string  `yaml:"CassandraPassword"`
+	CassandraUsername                            string  `yaml:"CassandraUsername"`
+	Command                                      string  `yaml:"Command"`
 	DataIntegrity                                bool    `yaml:"DataIntegrity"`
-	ReadProportion                               float64 `yaml:"ReadProportion"`
-	UpdateProportion                             float64 `yaml:"UpdateProportion"`
-	InsertProportion                             float64 `yaml:"InsertProportion"`
-	ScanProportion                               float64 `yaml:"ScanProportion"`
-	ReadModifyWriteProportion                    float64 `yaml:"ReadModifyWriteProportion"`
-	RequestDistribution                          string  `yaml:"RequestDistribution"`
-	ZeroPadding                                  int64   `yaml:"ZeroPadding"`
-	MinScanLength                                int64   `yaml:"MinScanLength"`
-	MaxScanLength                                int64   `yaml:"MaxScanLength"`
-	ScanLengthDistribution                       string  `yaml:"ScanLengthDistribution"`
-	InsertOrder                                  string  `yaml:"InsertOrder"`
+	DebugPprof                                   string  `yaml:"DebugPprof"`
+	DoTransactions                               bool    `yaml:"DoTransactions"`
+	DropData                                     bool    `yaml:"DropData"`
+	ExponentialFrac                              float64 `yaml:"ExponentialFrac"`
+	ExponentialPercentile                        float64 `yaml:"ExponentialPercentile"`
+	FieldCount                                   int64   `yaml:"FieldCount"`
+	FieldLength                                  int64   `yaml:"FieldLength"`
+	FieldLengthDistribution                      string  `yaml:"FieldLengthDistribution"`
+	FieldLengthHistogramFile                     string  `yaml:"FieldLengthHistogramFile"`
+    HelloTest                                    string  `yaml:"HelloTest"`
 	HotspotDataFraction                          float64 `yaml:"HotspotDataFraction"`
 	HotspotOpnFraction                           float64 `yaml:"HotspotOpnFraction"`
-	InsertionRetryLimit                          int64   `yaml:"InsertionRetryLimit"`
+	InsertCount                                  int64   `yaml:"InsertCount"`
 	InsertionRetryInterval                       int64   `yaml:"InsertionRetryInterval"`
-	ExponentialPercentile                        float64 `yaml:"ExponentialPercentile"`
-	ExponentialFrac                              float64 `yaml:"ExponentialFrac"`
-	DebugPprof                                   string  `yaml:"DebugPprof"`
-	Verbose                                      bool    `yaml:"Verbose"`
-	DropData                                     bool    `yaml:"DropData"`
-	Silence                                      bool    `yaml:"Silence"`
+	InsertionRetryLimit                          int64   `yaml:"InsertionRetryLimit"`
+	InsertOrder                                  string  `yaml:"InsertOrder"`
+	InsertProportion                             float64 `yaml:"InsertProportion"`
+	InsertStart                                  int64   `yaml:"InsertStart"`
 	KeyPrefix                                    string  `yaml:"KeyPrefix"`
+	Label                                        string  `yaml:"Label"`
 	LogInterval                                  int64   `yaml:"LogInterval"`
-	MeasurementType                              string  `yaml:"MeasurementType"`
-	MeasurementRawOutputFile                     string  `yaml:"MeasurementRawOutputFile"`
-	Command                                      string  `yaml:"Command"`
-	OutputStyle                                  string  `yaml:"OutputStyle"`
+	MaxExecutionTime                             int64   `yaml:"MaxExecutionTime"`
+	MaxScanLength                                int64   `yaml:"MaxScanLength"`
 	MeasurementHistogramPercentileExport         bool    `yaml:"HistogramPercentilesExport"`
 	MeasurementHistogramPercentileExportFilepath string  `yaml:"HistogramPercentilesExportFilepath"`
+	MeasurementRawOutputFile                     string  `yaml:"MeasurementRawOutputFile"`
+	MeasurementType                              string  `yaml:"MeasurementType"`
+	MinScanLength                                int64   `yaml:"MinScanLength"`
+	OperationCount                               int64   `yaml:"OperationCount"`
+	OutputStyle                                  string  `yaml:"OutputStyle"`
 	PasswordAuthenticator                        bool    `yaml:"PasswordAuthenticator"`
-	CassandraCluster                             string  `yaml:"CassandraCluster"`
-	CassandraKeyspace                            string  `yaml:"CassandraKeyspace"`
-	CassandraConnections                         int     `yaml:"CassandraConnections"`
-	CassandraUsername                            string  `yaml:"CassandraUsername"`
-	CassandraPassword                            string  `yaml:"CassandraPassword"`
+	ReadAllFields                                bool    `yaml:"ReadAllFields"`
+	ReadModifyWriteProportion                    float64 `yaml:"ReadModifyWriteProportion"`
+	ReadProportion                               float64 `yaml:"ReadProportion"`
+	RecordCount                                  int64   `yaml:"RecordCount"`
+	RequestDistribution                          string  `yaml:"RequestDistribution"`
+	ScanLengthDistribution                       string  `yaml:"ScanLengthDistribution"`
+	ScanProportion                               float64 `yaml:"ScanProportion"`
+	Silence                                      bool    `yaml:"Silence"`
+	Status                                       string  `yaml:"Status"`
+	TableName                                    string  `yaml:"TableName"`
+	Target                                       int64   `yaml:"Target"` // target operations per second
+	ThreadCount                                  int64   `yaml:"ThreadCount"`
+	UpdateProportion                             float64 `yaml:"UpdateProportion"`
+	Verbose                                      bool    `yaml:"Verbose"`
+	WarmUpTime                                   int64   `yaml:"WarmUpTime"`
+	Workload                                     string  `yaml:"Workload"`
+	WriteAllFields                               bool    `yaml:"WriteAllFields"`
+	ZeroPadding                                  int64   `yaml:"ZeroPadding"`
 }
 
 // NewConfig creates a new Config instance, populating it with values
@@ -69,62 +70,63 @@ type Config struct {
 func NewConfig(yamlFileName string) (*Config, error) {
 	// initialize with default values
 	defaultConfig := Config{
-		MeasurementHistogramPercentileExportFilepath: "./",
-		InsertStart:                          0,
-		InsertCount:                          0,
-		OperationCount:                       0,
-		RecordCount:                          0,
-		Workload:                             "",
-		ThreadCount:                          200,
-		Target:                               500,
-		MaxExecutionTime:                     0,
-		WarmUpTime:                           0,
-		DoTransactions:                       false,
-		Status:                               "",
-		Label:                                "",
 		BatchSize:                            1,
-		TableName:                            "usertable",
-		FieldCount:                           10,
-		FieldLengthDistribution:              "constant",
-		FieldLength:                          100,
-		FieldLengthHistogramFile:             "hist.txt",
-		ReadAllFields:                        true,
-		WriteAllFields:                       false,
+		CassandraCluster:                     "127.0.0.1: 9042",
+		CassandraConnections:                 2,
+		CassandraKeyspace:                    "test",
+		CassandraPassword:                    "",
+		CassandraUsername:                    "",
+		Command:                              "",
 		DataIntegrity:                        false,
-		ReadProportion:                       0.95,
-		UpdateProportion:                     0.05,
-		InsertProportion:                     0.0,
-		ScanProportion:                       0.0,
-		ReadModifyWriteProportion:            0.0,
-		RequestDistribution:                  "uniform",
-		ZeroPadding:                          1,
-		MinScanLength:                        1,
-		MaxScanLength:                        1000,
-		ScanLengthDistribution:               "uniform",
-		InsertOrder:                          "hashed",
+		DebugPprof:                           ": 6060",
+		DoTransactions:                       false,
+		DropData:                             false,
+		ExponentialFrac:                      0.8571428571,
+		ExponentialPercentile:                95.0,
+		FieldCount:                           10,
+		FieldLength:                          100,
+		FieldLengthDistribution:              "constant",
+		FieldLengthHistogramFile:             "hist.txt",
+        HelloTest:                                    "hello",
 		HotspotDataFraction:                  0.2,
 		HotspotOpnFraction:                   0.8,
-		InsertionRetryLimit:                  0,
+		InsertCount:                          0,
 		InsertionRetryInterval:               3,
-		ExponentialPercentile:                95.0,
-		ExponentialFrac:                      0.8571428571,
-		DebugPprof:                           ": 6060",
-		Verbose:                              false,
-		DropData:                             false,
-		Silence:                              true,
+		InsertionRetryLimit:                  0,
+		InsertOrder:                          "hashed",
+		InsertProportion:                     0.0,
+		InsertStart:                          0,
 		KeyPrefix:                            "user",
+		Label:                                "",
 		LogInterval:                          0,
-		MeasurementType:                      "histogram",
-		MeasurementRawOutputFile:             "",
-		Command:                              "",
-		OutputStyle:                          "plain",
+		MaxExecutionTime:                     0,
+		MaxScanLength:                        1000,
 		MeasurementHistogramPercentileExport: false,
-		PasswordAuthenticator:                false,
-		CassandraCluster:                     "127.0.0.1: 9042",
-		CassandraKeyspace:                    "test",
-		CassandraConnections:                 2,
-		CassandraUsername:                    "",
-		CassandraPassword:                    "",
+		MeasurementHistogramPercentileExportFilepath: "./",
+		MeasurementRawOutputFile:                     "",
+		MeasurementType:                              "histogram",
+		MinScanLength:                                1,
+		OperationCount:                               0,
+		OutputStyle:                                  "plain",
+		PasswordAuthenticator:                        false,
+		ReadAllFields:                                true,
+		ReadModifyWriteProportion:                    0.0,
+		ReadProportion:                               0.95,
+		RecordCount:                                  0,
+		RequestDistribution:                          "uniform",
+		ScanLengthDistribution:                       "uniform",
+		ScanProportion:                               0.0,
+		Silence:                                      true,
+		Status:                                       "",
+		TableName:                                    "usertable",
+		Target:                                       500,
+		ThreadCount:                                  200,
+		UpdateProportion:                             0.05,
+		Verbose:                                      false,
+		WarmUpTime:                                   0,
+		Workload:                                     "",
+		WriteAllFields:                               false,
+		ZeroPadding:                                  1,
 	}
 
 	// Read YAML file
--- b/tool/dev_scripts/add_property.sh
+++ b/tool/dev_scripts/add_property.sh
@@ -36,17 +36,18 @@ longest_prop_length=$(grep -o '^[[:blank:]]*[a-zA-Z_][a-zA-Z0-9_]*' "$GO_FILE" |
 
 # Spaces for new property
 spaces=$(add_spaces "$PROPERTY_NAME" "$longest_prop_length")
+spaces2=$(add_spaces "$PROPERTY_NAME" "$((longest_prop_length))")
 ymlspaces=$(add_spaces "$TYPE" 9)
 
 # Update the Go file
 # Add the new property to the Config struct with correct spacing
-sed -i '' "/type Config struct/a\\
+sed -i '' "/type Config struct {/a\\
     $PROPERTY_NAME$spaces$TYPE$ymlspaces\`yaml:\"$PROPERTY_NAME\"\`\\
 " "$GO_FILE"
 
 # Add the new property with its default value to the defaultConfig in NewConfig function, with a newline after
 sed -i '' "/defaultConfig := Config{/a\\
-        $PROPERTY_NAME: $DEFAULT_VALUE,\\
+        $PROPERTY_NAME:$spaces2$DEFAULT_VALUE,\\
 " "$GO_FILE"
 
 # Update the YAML file
@@ -56,22 +57,68 @@ echo "" >> "$YAML_FILE"
 
 echo "Property '$PROPERTY_NAME' added successfully."
 
-# Extract and sort the properties
-sorted_properties=$(sed -n '/type Config struct {/,/}/p' "$GO_FILE" | grep '^[[:blank:]]*[a-zA-Z_]' | sort)
 
-# Replace the struct with sorted properties
-awk -v sorted="$sorted_properties" '
-    /type Config struct {/ {
-        print;
-        print sorted;
-        skip = 1;
-    }
-    /}/ {
-        if (skip) {
-            print;
-            skip = 0;
-        }
-    }
-    !skip { print }' "$GO_FILE" > "$GO_FILE".tmp && mv "$GO_FILE".tmp "$GO_FILE"
+rm "${GO_FILE}.bak.sorted"
+rm "${GO_FILE}.bak"
+rm "${GO_FILE}.bak.sorted.sorted"
 
-echo "Properties in 'Config' struct sorted alphabetically."
+# Backup original file
+cp "$GO_FILE" "${GO_FILE}.bak"
+
+sort_from() {
+
+  local FILE="$1"
+  local from="$2"
+
+  # Get the start line of the Config struct
+  start_line=$(grep -n "$from" "$FILE" | cut -d: -f1)
+
+
+  # Get the end line of the Config struct
+  # This assumes the closing brace of the struct is at the start of the line
+  # Get the end line of the Config struct
+  end_line=$(awk -v start="$start_line" 'NR >= start {
+      if ($0 ~ /{/) { brace_count++ }
+      if ($0 ~ /}/) { brace_count-- }
+      if (brace_count == 0 && NR > start) { print NR; exit }
+  }' "$FILE")
+
+  start_line=$((start_line + 1))
+  end_line=$((end_line - 1))
+
+  echo "$start_line to $end_line"
+  # Temporary files for storing parts of the original file
+  TEMP_BEFORE="${FILE}_temp_before.txt"
+  TEMP_SORTED="${FILE}_temp_sorted.txt"
+  TEMP_AFTER="${FILE}_temp_after.txt"
+
+  # Extract lines before the sort range
+  T=$((start_line - 1))
+  head -n "$T" "$FILE" > "$TEMP_BEFORE"
+
+  # Extract and sort lines within the sort range
+  sed -n "${start_line},${end_line}p" "$FILE" | sort -f -b > "$TEMP_SORTED"
+
+  # Extract lines after the sort range
+  T=$((end_line + 1))
+  tail -n "+$T" "$FILE" > "$TEMP_AFTER"
+
+  # Concatenate the parts and overwrite the original file
+  cat "$TEMP_BEFORE" "$TEMP_SORTED" "$TEMP_AFTER" > "$FILE.sorted"
+
+  # Clean up temporary files
+  rm "$TEMP_BEFORE" "$TEMP_SORTED" "$TEMP_AFTER"
+
+  echo "File '$FILE' has been sorted from line $start_line to $end_line and updated."
+}
+
+sort_from "${GO_FILE}.bak" "type Config struct {"
+sort_from "${GO_FILE}.bak.sorted" "defaultConfig := Config{"
+cp "${GO_FILE}.bak.sorted.sorted" "$GO_FILE"
+rm "${GO_FILE}.bak.sorted.sorted"
+rm "${GO_FILE}.bak.sorted"
+rm "${GO_FILE}.bak"
+
+
+
+
